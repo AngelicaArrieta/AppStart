34aa6f1d9f1650d45227e08ed61af3ee62abb671
ANGEL ANIBAL OBANDO <90966843+Mintic2021@users.noreply.github.com>
2021-11-19 20:42:15 -0500

    actualizado

--- app/src/main/java/com/example/appnubedeluna/RegisterActivity.kt
+++ app/src/main/java/com/example/appnubedeluna/RegisterActivity.kt
@@ -1,4 +1,18 @@
 package com.example.appnubedeluna
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.textfield.TextInputLayout
+import java.util.regex.Pattern
+
+
 
 class RegisterActivity {
+
+
 }
